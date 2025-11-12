7d909945049be3affd80339d050a5a8af88c6c3c
adam wainkainen <awainikainen@gmail.com>
2025-11-12 03:03:29 -0500

    removed from generateAllCrud() so lesson is not defined two times

--- apps/api/src/schema.ts
+++ apps/api/src/schema.ts
@@ -1,7 +1,10 @@
 import { builder } from "./graphql/builder";
+import './graphql/lesson/lessonType'
+import './graphql/lesson/lesson.queries'
 import { generateAllCrud } from "./graphql/__generated__/autocrud";
 
-generateAllCrud();
+generateAllCrud({ exclude: ['Lesson'] });
+
 
 builder.queryType({});
 builder.mutationType({});
