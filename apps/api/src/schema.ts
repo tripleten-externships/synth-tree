fb287fb1df26da072e8fe1620f8eff61459db446
adam wainkainen <awainikainen@gmail.com>
2025-11-17 10:54:29 -0500

    :art:

--- apps/api/src/schema.ts
+++ apps/api/src/schema.ts
@@ -1,10 +1,10 @@
 import { builder } from "./graphql/builder";
-import './graphql/lesson/lessonType'
-import './graphql/lesson/lesson.queries'
+import "./graphql/lesson/lessonType";
+import "./graphql/lesson/lesson.queries";
 import { generateAllCrud } from "./graphql/__generated__/autocrud";
+import './graphql/lesson/lesson.mutations'
 
-generateAllCrud({ exclude: ['Lesson'] });
-
+generateAllCrud({ exclude: ["Lesson"] });
 
 builder.queryType({});
 builder.mutationType({});
