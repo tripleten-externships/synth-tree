ce44bd53631a880df761cd96591ff858a7d70743
alex brosovich <alexbrosovich@gmail.com>
2025-12-10 00:45:44 -0500

    fix(user error) re introducing changes from 9528eb2
    -graphQl imports from schema.ts

--- apps/api/src/schema.ts
+++ apps/api/src/schema.ts
@@ -1,7 +1,9 @@
 import { builder } from "./graphql/builder";
-import "./graphql/models/models.all";
-import "./graphql/mutations";
-import "./graphql/queries";
+import "./graphql/skillNode/skillNodeType";
+import "./graphql/skillNode/skillNode.queries";
+import "./graphql/lessonBlocks/blockType";
+import "./graphql/lessonBlocks/block.queries";
+import "./graphql/lessonBlocks/block.mutations";
 import { generateAllCrud } from "./graphql/__generated__/autocrud";
 
 // Auto-generate CRUD
