f7e4172e2dd0d2d918ee7082da9813a86188b3e3
djeon13 <djeon13@gmail.com>
2026-08-26 18:01:21 -0700

    add learner course tree query

---
+++ apps/client-frontend/src/graphql/queries/learnerCourseTree.ts
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client";
+
+export const LEARNER_COURSE_QUERY = gql`
+  query LearnerCourseTree($courseId: ID!) {
+    courseForLearner(id: $courseId) {
+      id
+      title
+      description
+      trees {
+        id
+        nodes {
+          id
+          title
+          posX
+          posY
+          isBoss
+          xpReward
+          lessonCount
+          prerequisites { dependsOnNodeId }
+          progressForViewer { status completedAt }
+        }
+      }
+    }
+  }
+`;
